5fc9f1ead622acdea7b5fd7812bb2851304d3016
Gerald Schade <geraldschade@gmx.de>
2018-01-04 01:40:52 +0100

    2.Commit

--- cdir.sh
+++ cdir.sh
@@ -6,8 +6,31 @@
 #SQLITE_LIBS = -lsqlite3 
 #XML_LIBS = -lxml2 
 #pkg-config --cflags --libs gtk+-2.0
-F=direkt; g++ $F.c -o$F \
+  #/home/schade/rogerj/wand/libroutermanager/libroutermanager_la-fax_phone.o \
+	#/home/schade/rogerj/wand/libroutermanager/libfaxophone/libroutermanager_la-faxophone.o \
+	#/home/schade/rogerj/wand/libroutermanager/libfaxophone/libroutermanager_la-phone.o \
+	#/home/schade/rogerj/wand/libroutermanager/libfaxophone/libroutermanager_la-fax.o \
+	#/home/schade/rogerj/wand/libroutermanager/libfaxophone/libroutermanager_la-sff.o \
+	#/home/schade/autofax/capi20_copy/libcapi20_la-capi20.o \
+	#/home/schade/autofax/capi20_copy/libcapi20dyn.a \
+	#/home/schade/rogerj/wand/libroutermanager/libfaxophone/.libs/libroutermanager_la-faxophone.o \
+F=direkt; g++ $F.c \
+  -I/home/schade/rogerj/wand \
+  -I/home/schade/rogerj/wand/libroutermanager \
+	/home/schade/rogerj/wand/libroutermanager/libfaxophone/faxophone.c \
+	/home/schade/rogerj/wand/libroutermanager/libfaxophone/phone.c \
+	/home/schade/rogerj/wand/libroutermanager/libfaxophone/fax.c \
+	/home/schade/rogerj/wand/libroutermanager/audio.c \
+	/home/schade/rogerj/wand/libroutermanager/logging.c \
+	/home/schade/rogerj/wand/libroutermanager/appobject-emit.c \
+	/home/schade/rogerj/wand/libroutermanager/libfaxophone/sff.c \
+	/home/schade/rogerj/wand/libroutermanager/libfaxophone/isdn-convert.c \
+	/home/schade/autofax/capi20_copy/libcapi20_la-capifunc.o \
+	/home/schade/rogerj/wand/libroutermanager/.libs/libroutermanager.so.0.0.0 \
+	/home/schade/autofax/capi20_copy/.libs/libcapi20.a \
   $(pkg-config --cflags --libs libsoup-2.4) \
+	-ltiff -lspandsp -lrt -ldl -lsndfile \
+	-o$F \
   >fehler 2>&1 \
   &&{ ./$F;:;} \
   ||vim +2 fehler
--- direkt.c
+++ direkt.c
@@ -5,6 +5,22 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <capiutils.h>
+#include "/home/schade/rogerj/wand/libroutermanager/libfaxophone/fax.h"
+#include "/home/schade/rogerj/wand/libroutermanager/libfaxophone/phone.h"
+#include "/home/schade/rogerj/wand/libroutermanager/libfaxophone/faxophone.h"
+#include "/home/schade/rogerj/wand/libroutermanager/fax_phone.h"
+#include "/home/schade/rogerj/wand/libroutermanager/audio.h"
+#include "/home/schade/rogerj/wand/libroutermanager/appobject.h"
+#include "/home/schade/rogerj/wand/libroutermanager/appobject-emit.h"
+#include "/home/schade/rogerj/wand/libroutermanager/logging.h"
+#include "/home/schade/rogerj/wand/libroutermanager/net_monitor.h"
+#include "/home/schade/rogerj/wand/libroutermanager/routermanager.h"
+/*
+#include "/home/schade/rogerj/wand/libroutermanager/router.h"
+#include "/home/schade/rogerj/wand/libroutermanager/routermanager.h"
+*/
+
 std::string nix="";
 
 std::string xmlex(std::string data,std::string lim,std::string lim2=nix,size_t *p2p=0)
@@ -580,12 +596,621 @@ set:
   return;
 }
 
+static gint log_level = 0;
+
+static void (*logging)(gint level, const gchar *text) = NULL;
+
+/**
+ * \brief Dump spandsp messages
+ * \param level spandsp loglevel
+ * \param text message text
+ */
+static void spandsp_msg_log(gint level, const gchar *text)
+{
+	g_debug("%s", text);
+}
+
+/**
+ * \brief Get total pages in TIFF file
+ * \param file filename
+ * \return number of pages
+ */
+static int get_tiff_total_pages(const char *file)
+{
+	TIFF *tiff_file;
+	int max;
+
+	if ((tiff_file = TIFFOpen(file, "r")) == NULL) {
+		return -1;
+	}
+
+	max = 0;
+	while (TIFFSetDirectory(tiff_file, (tdir_t) max)) {
+		max++;
+	}
+
+	TIFFClose(tiff_file);
+
+	return max;
+}
+
+/**
+ * \brief Initialize spandsp
+ * \param tiff_file tiff file
+ * \param sending sending flag
+ * \param modem supported modem
+ * \param ecm error correction mode flag
+ * \param lsi lsi
+ * \param local_header_info local header
+ * \param connection capi connection poiner
+ * \return error code
+ */
+gint spandsp_init_hier(const gchar *tiff_file, gboolean sending, gchar modem, gchar ecm, const gchar *lsi, const gchar *local_header_info, struct capi_connection *connection)
+{
+	t30_state_t *t30;
+	logging_state_t *log_state;
+	gint supported_resolutions = 0;
+	gint supported_image_sizes = 0;
+	gint supported_modems = 0;
+	struct fax_status *status = (fax_status*)connection->priv;
+
+	status->fax_state = fax_init(NULL, sending);
+	g_debug("status->fax_state: %p", status->fax_state);
+
+	fax_set_transmit_on_idle(status->fax_state, TRUE);
+	fax_set_tep_mode(status->fax_state, FALSE);
+
+	t30 = fax_get_t30_state(status->fax_state);
+
+	/* Supported resolutions */
+	supported_resolutions = 0;
+	supported_resolutions |= T30_SUPPORT_STANDARD_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_FINE_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_SUPERFINE_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_R8_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_R16_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_300_300_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_400_400_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_600_600_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_1200_1200_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_300_600_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_400_800_RESOLUTION;
+	supported_resolutions |= T30_SUPPORT_600_1200_RESOLUTION;
+
+	/* Supported image sizes */
+	supported_image_sizes = 0;
+	supported_image_sizes |= T30_SUPPORT_215MM_WIDTH;
+	supported_image_sizes |= T30_SUPPORT_255MM_WIDTH;
+	supported_image_sizes |= T30_SUPPORT_303MM_WIDTH;
+	supported_image_sizes |= T30_SUPPORT_UNLIMITED_LENGTH;
+	supported_image_sizes |= T30_SUPPORT_A4_LENGTH;
+	supported_image_sizes |= T30_SUPPORT_US_LETTER_LENGTH;
+	supported_image_sizes |= T30_SUPPORT_US_LEGAL_LENGTH;
+
+	/* Supported modems */
+	supported_modems = 0;
+	if (modem > 0) {
+		supported_modems |= T30_SUPPORT_V27TER;
+		if (modem > 1) {
+			supported_modems |= T30_SUPPORT_V29;
+		}
+		if (modem > 2) {
+			supported_modems |= T30_SUPPORT_V17;
+		}
+#if defined(T30_SUPPORT_V34)
+		if (modem > 3) {
+			supported_modems |= T30_SUPPORT_V34;
+		}
+#endif
+	}
+
+	t30_set_supported_modems(t30, supported_modems);
+
+	/* Error correction */
+	if (ecm) {
+		/* Supported compressions */
+#if defined(SPANDSP_SUPPORT_T85)
+		t30_set_supported_compressions(t30, T30_SUPPORT_T4_1D_COMPRESSION | T30_SUPPORT_T4_2D_COMPRESSION | T30_SUPPORT_T6_COMPRESSION | T30_SUPPORT_T85_C
+#else
+		t30_set_supported_compressions(t30, T30_SUPPORT_T4_1D_COMPRESSION | T30_SUPPORT_T4_2D_COMPRESSION | T30_SUPPORT_T6_COMPRESSION);
+#endif
+
+		t30_set_ecm_capability(t30, ecm);
+	}
+
+	t30_set_supported_t30_features(t30, T30_SUPPORT_IDENTIFICATION | T30_SUPPORT_SELECTIVE_POLLING | T30_SUPPORT_SUB_ADDRESSING);
+	t30_set_supported_resolutions(t30, supported_resolutions);
+	t30_set_supported_image_sizes(t30, supported_image_sizes);
+
+	/* spandsp loglevel */
+	if (log_level >= 1) {
+		log_state = t30_get_logging_state(t30);
+		span_log_set_level(log_state, 0xFFFFFF);
+
+		if (!logging) {
+			logging = spandsp_msg_log;
+		}
+
+		span_log_set_message_handler(log_state, logging);
+	}
+
+	if (lsi) {
+		t30_set_tx_ident(t30, lsi);
+	}
+	if (local_header_info) {
+		t30_set_tx_page_header_info(t30, local_header_info);
+	}
+
+	if (sending == TRUE) {
+		t30_set_tx_file(t30, tiff_file, -1, -1);
+		status->page_total = get_tiff_total_pages(tiff_file);
+	} else {
+		t30_set_rx_file(t30, tiff_file, -1);
+	}
+	/*
+	t30_set_phase_b_handler(t30, phase_handler_b, (void *) connection);
+	t30_set_phase_d_handler(t30, phase_handler_d, (void *) connection);
+	t30_set_phase_e_handler(t30, phase_handler_e, (void *) connection);
+
+	t30_set_real_time_frame_handler(t30, real_time_frame_handler, (void *) connection);
+*/
+	return 0;
+}
+
+// aus fax.c
+gint spandsp_init(const gchar *tiff_file, gboolean sending, gchar modem, gchar ecm, const gchar *lsi, const gchar *local_header_info, struct capi_connection *connection);
+
+/**
+ * \brief Send Fax
+ * \param tiff_file The Tiff file to send
+ * \param modem 0-3 (2400-14400)
+ * \param ecm Error correction mode (on/off)
+ * \param controller The controller for sending the fax
+ * \param src_no MSN
+ * \param trg_no Target fax number
+ * \param lsi Fax ident
+ * \param local_header_info Fax header line
+ * \param call_anonymous Send fax anonymous
+ * \return error code
+ */
+struct capi_connection *fax_send_hier(gchar *tiff_file, gint modem, gint ecm, gint controller, gint cip, const gchar *src_no, const gchar *trg_no, const gchar *lsi, const gchar *local_header_info, gint call_anonymous)
+{
+  printf("Beginn fax_send: %s, %d, %d, %d, %d, %s, %s, %s, %s, %d\n",tiff_file,modem,ecm,controller,cip,src_no,trg_no,lsi,local_header_info,call_anonymous);
+	struct fax_status *status;
+	struct capi_connection *connection;
+
+	g_debug("tiff: %s, modem: %d, ecm: %s, controller: %d, src: %s, trg: %s, ident: %s, header: %s, anonymous: %d)", tiff_file, modem, ecm ? "on" : "off", controller, src_no, trg_no, (lsi != NULL ? lsi : "(null)"), (local_header_info != NULL ? local_header_info : "(null)"), call_anonymous);
+
+	//status = g_slice_new0(struct fax_status);
+	status = (fax_status*)malloc(sizeof(struct fax_status));
+	memset(status, 0, sizeof(struct fax_status));
+
+	status->phase = IDLE;
+	status->error_code = -1;
+	status->sending = 1;
+	status->manual_hookup = 0;
+	status->modem = modem;
+	status->ecm = ecm;
+	snprintf(status->header, sizeof(status->header), "%s", local_header_info);
+	snprintf(status->ident, sizeof(status->ident), "%s", lsi);
+	snprintf(status->src_no, sizeof(status->src_no), "%s", src_no);
+	snprintf(status->trg_no, sizeof(status->trg_no), "%s", trg_no);
+	snprintf(status->tiff_file, sizeof(status->tiff_file), "%s", tiff_file);
+
+	connection = capi_call(controller, src_no, trg_no, (guint) call_anonymous, SESSION_FAX, cip, 1, 1, 0, NULL, NULL, NULL);
+	if (connection) {
+		status->connection = connection;
+		connection->priv = status;
+		spandsp_init(status->tiff_file, TRUE, status->modem, status->ecm, status->ident, status->header, connection);
+	}
+
+	return connection;
+}
+
+gboolean faxophone_connect(gpointer user_data);
+gboolean faxophone_disconnect(gpointer user_data);
+
+/**
+ * \brief Connection ring handler
+ * \param connection capi connection structure
+ */
+gboolean connection_ring_idle(gpointer data)
+{
+	struct capi_connection *capi_connection = (struct capi_connection*)data;
+	struct connection *connection;
+
+	active_capi_connection = capi_connection;
+
+	g_debug("connection_ring() src %s trg %s", capi_connection->source, capi_connection->target);
+	connection = connection_find_by_number(capi_connection->source);
+#if ACCEPT_INTERN
+	if (!connection && !strncmp(capi_connection->source, "**", 2)) {
+		connection = connection_add_call(981, CONNECTION_TYPE_INCOMING, capi_connection->source, capi_connection->target);
+	}
+#endif
+
+	g_debug("connection_ring() connection %p", connection);
+	if (connection) {
+		g_debug("connection_ring() set capi_connection %p", capi_connection);
+		connection->priv = capi_connection;
+
+		emit_connection_notify(connection);
+	}
+
+	return G_SOURCE_REMOVE;
+}
+
+void connection_ring(struct capi_connection *capi_connection)
+{
+	g_idle_add(connection_ring_idle, capi_connection);
+}
+
+
+/**
+ * \brief Connection code handler
+ * \param connection capi connection structure
+ * \param code dtmf code
+ */
+void connection_code(struct capi_connection *connection, gint code)
+{
+	g_debug("connection_code(): code 0x%x", code);
+}
+
+/**
+ * \brief Connection status handlers - emits connection-status signal
+ * \param connection capi connection structure
+ * \param status status code
+ */
+void connection_status(struct capi_connection *connection, gint status)
+{
+	printf("!!!!!!!!Anfang connection_status\n");
+	emit_connection_status(status, connection);
+}
+
+gboolean connection_established_idle(gpointer data)
+{
+	struct capi_connection *connection = (struct capi_connection*)data;
+
+	emit_connection_established(connection);
+
+	return G_SOURCE_REMOVE;
+}
+
+void connection_established(struct capi_connection *connection)
+{
+	g_idle_add(connection_established_idle, connection);
+}
+
+gboolean connection_terminated_idle(gpointer data)
+{
+	struct capi_connection *connection = (struct capi_connection*)data;
+
+	emit_connection_terminated(connection);
+
+	return G_SOURCE_REMOVE;
+}
+
+void connection_terminated(struct capi_connection *connection)
+{
+	g_idle_add(connection_terminated_idle, connection);
+}
+
+static struct session *session = NULL;
+struct session_handlers session_handlers = {
+	audio_open, /* audio_open */
+	audio_read, /* audio_read */
+	audio_write, /* audio_write */
+	audio_close, /* audio_close */
+
+	connection_established, /* connection_established */
+	connection_terminated, /* connection_terminated */
+	connection_ring, /* connection_ring */
+	connection_code, /* connection_code */
+	connection_status, /* connection_status */
+};
+
+/**
+ * \brief Network disconnect callback
+ * \param user_data faxophone plugin pointer
+ * \return TRUE
+ */
+gboolean faxophone_disconnect(gpointer user_data)
+{
+	faxophone_close(TRUE);
+	return TRUE;
+}
+
+/**
+ * \brief Faxophone connect
+ * \param user_data faxophone plugin pointer
+ * \return error code
+ */
+gboolean faxophone_connect_hier(gpointer user_data)
+{
+//	struct profile *profile = profile_get_active();
+	gboolean retry = TRUE;
+	gchar* host=(gchar*)"fritz.box"; // router_get_host(profile);
+	gint phonecontr=4; // g_settings_get_int(profile->settings, "phone-controller");
+	printf("Beginn faxophone_connect_hier, host: %s, phone-controller: %i\n",host,phonecontr);
+again:
+	session = faxophone_init(&session_handlers, host, phonecontr + 1);
+/*
+	if (!session && retry) {
+		// Maybe the port is closed, try to activate it and try again 
+		router_dial_number(profile, PORT_ISDN1, "#96*3*");
+		g_usleep(G_USEC_PER_SEC * 2);
+		retry = FALSE;
+		goto again;
+	}
+*/
+	return session != NULL;
+}
+
+// aus main_cli.c
+#include <config.h>
+#undef _
+#define _(x) x
+static gboolean debug = FALSE;
+static gboolean journal = FALSE;
+static gboolean sendfax = FALSE;
+static gboolean call = FALSE;
+static gchar *file_name = NULL;
+static gchar *number = NULL;
+static GOptionEntry entries[] = {
+	{"debug", 'd', 0, G_OPTION_ARG_NONE, &debug, "Enable debug messages", NULL},
+	{"journal", 'j', 0, G_OPTION_ARG_NONE, &journal, "Prints journal", NULL},
+	{"sendfax", 's', 0, G_OPTION_ARG_NONE, &sendfax, "Send fax", NULL},
+	{"file", 'f', 0, G_OPTION_ARG_STRING, &file_name, "PDF/PS file", NULL},
+	{"number", 'n', 0, G_OPTION_ARG_STRING, &number, "Remote phone number", NULL},
+	{"call", 'c', 0, G_OPTION_ARG_NONE, &call, "Call number", NULL},
+	{NULL}
+};
+/** Internal main loop */
+GMainLoop *main_loop = NULL;
+static gboolean success = FALSE;
+
+/**
+ * \brief FAX connection status - show status message
+ * \param object appobject
+ * \param status fax connection status
+ * \param connection capi connection pointer
+ * \param user_data user data pointer (NULL)
+ */
+void fax_connection_status_cb(AppObject *object, gint status, struct capi_connection *connection, gpointer user_data)
+{
+	struct fax_status *fax_status;
+	gchar buffer[256];
+	printf("Begin fax_connection_status_cb\n");
+
+	fax_status = (struct fax_status*)connection->priv;
+	if (!fax_status) {
+		g_warning("No status available");
+		return;
+	}
+
+	if (!status && !fax_status->done) {
+		switch (fax_status->phase) {
+		case PHASE_B:
+			g_debug("Ident: %s", fax_status->remote_ident);
+			snprintf(buffer, sizeof(buffer), "%d/%d", fax_status->page_current, fax_status->page_total);
+
+			g_message(_("Transfer starting:"));
+			g_message("%s", buffer);
+			break;
+		case PHASE_D:
+			snprintf(buffer, sizeof(buffer), "%d", fax_status->page_current);
+			g_message(_("Transferring page"));
+			g_message("%s", buffer);
+			break;
+		case PHASE_E:
+			if (!fax_status->error_code) {
+				g_message("%s", "Fax transfer successful");
+				success = TRUE;
+			} else {
+				g_message("%s", "Fax transfer failed");
+				success = FALSE;
+			}
+			phone_hangup(connection);
+			fax_status->done = TRUE;
+			g_main_loop_quit(main_loop);
+			break;
+		default:
+			g_debug("Unhandled phase (%d)", fax_status->phase);
+			break;
+		}
+	} else if (status == 1) {
+		float percentage = 0.0f;
+		gchar text[6];
+		int percent = 0;
+		static int old_percent = 0;
+
+		percentage = (float) fax_status->bytes_sent / (float) fax_status->bytes_total;
+
+		if (percentage > 1.0f) {
+			percentage = 1.0f;
+		}
+
+		percent = percentage * 100;
+		if (old_percent == percent) {
+			return;
+		}
+		old_percent = percent;
+
+		snprintf(text, sizeof(text), "%d%%", percent);
+		g_message("Transfer at %s", text);
+	}
+}
+
+/**
+ * \brief CAPI connection established callback - just print message
+ * \param object appobject
+ * \param connection capi connection pointer
+ * \param user_data user data pointer (NULL)
+ */
+static void capi_connection_established_cb(AppObject *object, struct capi_connection *connection, gpointer user_data)
+{
+	g_message(_("Connected"));
+}
+
+/**
+ * \brief CAPI connection terminated callback - just print message
+ * \param object appobject
+ * \param connection capi connection pointer
+ * \param user_data user data pointer (NULL)
+ */
+static void capi_connection_terminated_cb(AppObject *object, struct capi_connection *connection, gpointer user_data)
+{
+	g_message(_("Disconnected"));
+	g_main_loop_quit(main_loop);
+}
+
+// aus routermanager.c
+/**
+ * \brief Create routermanager
+ * \param debug enable debug output
+ * \return TRUE on success, FALSE on error
+ */
+gboolean routermanager_new_hier(gboolean debug, GError **error)
+{
+	gchar *dir;
+
+#if !GLIB_CHECK_VERSION(2, 36, 0)
+	/* Init g_type */
+	g_type_init();
+#endif
+
+	/* Init directory path */
+	// init_directory_paths();
+
+	/* Initialize logging system */
+	log_init(debug);
+
+	/* Say hello */
+	g_debug("%s %s", PACKAGE_NAME, PACKAGE_VERSION);
+
+	/* Create routermanager data & cache directory */
+	dir = g_build_filename(g_get_user_data_dir(), "routermanager", NULL);
+	g_mkdir_with_parents(dir, 0700);
+	g_free(dir);
+
+	dir = g_build_filename(g_get_user_cache_dir(), "routermanager", NULL);
+	g_mkdir_with_parents(dir, 0700);
+	g_free(dir);
+
+	/* Create main app object (signals) */
+	app_object = app_object_new();
+	g_assert(app_object != NULL);
+
+	return TRUE;
+}
+/**
+ * \brief Initialize routermanager
+ * \return TRUE on success, FALSE on error
+ */
+gboolean routermanager_init_hier(GError **error)
+{
+	/* Init filter */
+	// filter_init();
+
+	/* Init fax printer */
+	/*
+	if (!fax_printer_init(error)) {
+		return FALSE;
+	}
+	*/
+
+	/* Initialize network */
+	net_init();
+
+	/* Load plugins depending on ui (router, audio, address book, reverse lookup...) */
+	// routermanager_plugins_add_search_path(get_plugin_dir());
+
+	/* Initialize plugins */
+	// plugins_init();
+
+	/* Check password manager */
+	/*
+	if (!password_manager_get_plugins()) {
+		g_set_error(error, RM_ERROR, RM_ERROR_ROUTER, "%s", "No password manager plugins active");
+		return FALSE;
+	}
+	*/
+
+	/* Initialize router */
+	/*
+	if (!router_init()) {
+		g_set_error(error, RM_ERROR, RM_ERROR_ROUTER, "%s", "Failed to initialize router");
+		return FALSE;
+	}
+	*/
+
+	/* Initialize profiles */
+	// profile_init();
+
+	/* Initialize network monitor */
+	// net_monitor_init();
+
+	return TRUE;
+}
 
 int main(int argc, char** argv)
 {
-  fbcl fb;
-  fb.fb_login_05_50();
-  std::cout<<"eingel: "<<fb.eingel<<std::endl;
+	GError *error = NULL;
+	GOptionContext *context;
+	gchar *tiff = NULL;
+	int ret = 0;
+
+#if !GLIB_CHECK_VERSION(2, 36, 0)
+	/* Init g_type */
+	g_type_init();
+#endif
+
+	context = g_option_context_new("-");
+	g_option_context_add_main_entries(context, entries, GETTEXT_PACKAGE);
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
+		g_print("option parsing failed: %s\n", error->message);
+		exit(1);
+	}
+	routermanager_new(debug, NULL);
+	/* Initialize routermanager */
+
+	routermanager_init_hier(NULL);
+	//faxophone_setup();
+	static gconstpointer net_event;
+	net_event = net_add_event(faxophone_connect_hier, faxophone_disconnect, NULL);
+
+	/* Only show messages >= INFO */
+	log_set_level(G_LOG_LEVEL_INFO);
+
 //  fb.loadj();
-  fb.fb_get_settings_05_50();
+//  fb.fb_get_settings_05_50();
+		g_signal_connect(app_object, "connection-status", G_CALLBACK(fax_connection_status_cb), NULL);
+		g_signal_connect(app_object, "connection-established", G_CALLBACK(capi_connection_established_cb), NULL);
+		g_signal_connect(app_object, "connection-terminated", G_CALLBACK(capi_connection_terminated_cb), NULL);
+
+	system("gs -q -dNOPAUSE -dSAFER -dBATCH -sDEVICE=tiffg32d -sPAPERSIZE=a4 -dFIXEDMEDIA -r204x98 -sOutputFile=t0.pdf.tif ~/rogerj/wand/t0.pdf");
+	gpointer user_data;
+printf("4 !!!!!!!!!!!!!!!!!!!\n");
+	faxophone_connect_hier(user_data);
+printf("5 !!!!!!!!!!!!!!!!!!!\n");
+//	exit(0);
+	// aus fax_dial
+	struct capi_connection * conn=fax_send_hier((gchar*)"t0.pdf.tif",2,1,5,4,(gchar*)"6150166",(gchar*)"619712",(gchar*)"+496150166",(gchar*)"G.Schade",0);
+	/* Create and start g_main_loop */
+
+	printf("Vor main_loop\n");
+  // fax_transfer
+	main_loop = g_main_loop_new(NULL, FALSE);
+	g_main_loop_run(main_loop);
+	printf("Nach main_loop\n");
+
+	/* Shutdown routermanager */
+	//routermanager_shutdown();
+	/* Destroy app_object */
+	g_clear_object(&app_object);
+
+	/* Shutdown logging */
+	log_shutdown();
+
+	faxophone_disconnect(user_data);
 }
